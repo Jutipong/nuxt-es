3a132d276c1e8d36fb78c950fb4a04d7ce6965d6
ohm <techcom.ec20@gmail.com>
2020-09-14 15:55:20 +0700

    clean code

--- pages/demo01.vue
+++ pages/demo01.vue
@@ -32,30 +32,9 @@ export default {
       endDate: null,
     };
   },
-  computed: {
-    computedDateFormatted() {
-      return this.formatDate(this.date);
-    },
-  },
-  watch: {
-    date(val) {
-      this.dateFormatted = this.formatDate(this.date);
-    },
-  },
-  methods: {
-    formatDate(date) {
-      if (!date) return null;
-
-      const [year, month, day] = date.split('-');
-      return `${month}/${day}/${year}`;
-    },
-    parseDate(date) {
-      if (!date) return null;
-
-      const [month, day, year] = date.split('/');
-      return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
-    },
-  },
+  computed: {},
+  methods: {},
+  watch: {},
   destroyed() {
     this.destroyed();
   },
