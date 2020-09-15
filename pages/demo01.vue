9d5be141200e3b9d2cc88b568493429d4074f2c9
ohm <techcom.ec20@gmail.com>
2020-09-15 15:09:59 +0700

    Initial numbercomponent

--- pages/demo01.vue
+++ pages/demo01.vue
@@ -13,18 +13,18 @@
             <DatePicker :locale="'th'" :startDate="startDate" :label="'EndDate'" v-model="endDate"></DatePicker>
           </v-col>
           <v-col cols="12" md="4" lg="4">
-            <v-text-field v-model="numberValue" hide-details single-line type="number" />
+            <CurrencyInput :options="options" v-model="amount"></CurrencyInput>
           </v-col>
         </v-row>
         <v-row dense>
           <v-col cols="12" md="4" lg="4">
-            <v-text-field name="Name" label="Name" autocomplete="off"></v-text-field>
+            <v-text-field name="Name" label="Name" autocomplete="off" clearable></v-text-field>
           </v-col>
           <v-col cols="12" md="4" lg="4">
-            <v-text-field name="Last" label="Last" autocomplete="off"></v-text-field>
+            <v-text-field name="Last" label="Last" autocomplete="off" clearable></v-text-field>
           </v-col>
           <v-col cols="12" md="4" lg="4">
-            <v-autocomplete v-model="status" :items="items" multiple label="Status" clearable></v-autocomplete>
+            <v-autocomplete v-model="status" :items="items" label="Status" clearable></v-autocomplete>
           </v-col>
           <v-col cols="12" class="text-right">
             <v-btn class="btn-df" tile color="warning" elevation="1">Clear <v-icon>mdi-reload</v-icon></v-btn>
@@ -39,18 +39,30 @@
 
 <script>
 import DatePicker from '@/components/datePicker';
+import currencyInput from '@/components/currencyInput';
 
 export default {
   components: {
     DatePicker,
+    currencyInput,
   },
   data() {
     return {
-      numberValue: null,
+      amount: 1234,
       startDate: null,
       endDate: null,
       items: ['foo', 'bar', 'fizz', 'buzz'],
       status: null,
+      options: {
+        currency: null,
+        locale: 'en',
+        precision: 2,
+        distractionFree: {
+          hideNegligibleDecimalDigits: false,
+          hideCurrencySymbol: false,
+          hideGroupingSymbol: false,
+        },
+      },
     };
   },
   computed: {},
