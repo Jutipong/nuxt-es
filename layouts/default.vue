4efe95bacadceec40eff35c3b66c3ade6488a7c2
ohm <techcom.ec20@gmail.com>
2020-09-15 13:44:39 +0700

    update code

--- layouts/default.vue
+++ layouts/default.vue
@@ -105,7 +105,7 @@ export default {
 };
 </script>
 <style>
-.wdf {
+.btn-df {
   min-width: 125px !important;
 }
 .v-card__text {
--- pages/demo01.vue
+++ pages/demo01.vue
@@ -2,7 +2,7 @@
   <!-- <v-form> -->
   <v-container fluid>
     <v-card tile outlined elevation="2">
-      <v-toolbar flat dense><v-icon>mdi-magnify</v-icon>Search</v-toolbar>
+      <v-toolbar flat dense><v-icon>mdi-magnify</v-icon></v-toolbar>
       <v-divider></v-divider>
       <v-card-text>
         <v-row dense>
@@ -27,8 +27,8 @@
             <v-autocomplete v-model="status" :items="items" multiple label="Status" clearable></v-autocomplete>
           </v-col>
           <v-col cols="12" class="text-right">
-            <v-btn class="wdf" tile color="warning" elevation="1">Clear <v-icon>mdi-reload</v-icon></v-btn>
-            <v-btn class="wdf" tile color="primary" elevation="1"> Search <v-icon>mdi-magnify</v-icon> </v-btn>
+            <v-btn class="btn-df" tile color="warning" elevation="1">Clear <v-icon>mdi-reload</v-icon></v-btn>
+            <v-btn class="btn-df" tile color="primary" elevation="1"> Search <v-icon>mdi-magnify</v-icon> </v-btn>
           </v-col>
         </v-row>
       </v-card-text>
@@ -56,9 +56,6 @@ export default {
   computed: {},
   methods: {},
   watch: {},
-  destroyed() {
-    // this.destroy();
-  },
 };
 </script>
 
